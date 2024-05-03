8aa99c2ea5f91c1d44554656d1c13138986e7a54
Jay Rogers <jay@521dimensions.com>
2024-05-02 19:58:33 -0500

    chore: Update new.sh script with improved logging configuration

--- new.sh
+++ new.sh
@@ -3,5 +3,5 @@ set -e
 PHP_IMAGE="serversideup/php:8.3-cli"
 project_dir=${1:-laravel}
 docker pull $PHP_IMAGE
-docker run --rm -w /var/www/html -v $(pwd):/var/www/html --user "${SPIN_USER_ID}:${SPIN_GROUP_ID}" -e "LOG_LEVEL_OUTPUT=off" $PHP_IMAGE composer --no-cache create-project laravel/laravel "$@"
-docker run --rm -v "$project_dir:/var/www/html" -e "LOG_LEVEL_OUTPUT=off" $PHP_IMAGE composer --working-dir=/var/www/html/ require serversideup/spin --dev
+docker run --rm -w /var/www/html -v $(pwd):/var/www/html --user "${SPIN_USER_ID}:${SPIN_GROUP_ID}" -e "LOG_OUTPUT_LEVEL=off" $PHP_IMAGE composer --no-cache create-project laravel/laravel "$@"
+docker run --rm -v "$project_dir:/var/www/html" -e "LOG_OUTPUT_LEVEL=off" $PHP_IMAGE composer --working-dir=/var/www/html/ require serversideup/spin --dev
