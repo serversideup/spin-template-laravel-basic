b447cfd0af1ee7841e9d1c7492015dd299524ebe
Jay Rogers <jay@521dimensions.com>
2024-05-03 17:47:47 -0500

    Remove debug

--- new.sh
+++ new.sh
@@ -1,7 +1,6 @@
 #/bin/env bash
 set -e
 PHP_IMAGE="serversideup/php:8.3-cli"
-echo "$@"
 project_dir=${1:-"laravel"}
 docker pull $PHP_IMAGE
 docker run --rm -w /var/www/html -v $(pwd):/var/www/html --user "${SPIN_USER_ID}:${SPIN_GROUP_ID}" -e "SHOW_WELCOME_MESSAGE=false" $PHP_IMAGE composer --no-cache create-project laravel/laravel "$@"
