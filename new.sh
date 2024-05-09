69f244bbc727a69c87fd84034175207ecaeb11a0
Jay Rogers <jay@521dimensions.com>
2024-05-09 13:27:59 -0500

    Update PHP image version to 8.3-cli

--- new.sh
+++ new.sh
@@ -1,6 +1,6 @@
 #/bin/env bash
 set -e
-PHP_IMAGE="serversideup/php:8.3-cli-alpine"
+PHP_IMAGE="serversideup/php:8.3-cli"
 project_dir=${1:-"laravel"}
 docker pull $PHP_IMAGE
 docker run --rm -w /var/www/html -v $(pwd):/var/www/html --user "${SPIN_USER_ID}:${SPIN_GROUP_ID}" -e "SHOW_WELCOME_MESSAGE=false" $PHP_IMAGE composer --no-cache create-project laravel/laravel "$@"
