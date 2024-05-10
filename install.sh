1d9bf5000e4bf07a070dbc2d951fdafe12a9dc57
Jay Rogers <jay@521dimensions.com>
2024-05-10 14:28:41 -0500

    chore: Update install.sh script to use alpine-based PHP image and set project directory for new installations

--- install.sh
+++ install.sh
@@ -4,22 +4,37 @@ set -e # Exit on error
 ###############################################
 # Prepare enviornment
 ###############################################
+
+# Make sure the image is up to date before running
 docker_image="serversideup/php:8.3-cli"
+docker pull $docker_image
 
 # Save anything passed to the script as an array
 framework_args=("$@")
 
-# Check if any arguments were passed
-if [ ${#framework_args[@]} -eq 0 ]; then
-  # Set default directory to Laravel
-  project_dir="laravel"
-else
-  # Grab the first argument as the project directory (this is what Laravel does)
-  project_dir="${framework_args[0]}"
-fi
+###############################################
+# Configure "spin_project_directory" for new installs
+###############################################
 
-# Make sure the image is up to date before running
-docker pull $docker_image
+# Spin needs to know what the project directory is
+# for new installations. You MUST export this variable
+# to Spin so it can complete the installation correctly.
+
+if [ "$SPIN_ACTION" == "new" ]; then
+
+  # Check if any framework arguments were passed
+  if [ ${#framework_args[@]} -eq 0 ]; then
+    # Set default directory to Laravel
+    spin_project_directory="laravel"
+  else
+    # Grab the first argument as the project directory (this is what Laravel does)
+    spin_project_directory="${framework_args[0]}"
+  fi
+
+  # Export this variable back to Spin
+  export spin_project_directory
+
+fi
 
 ###############################################
 # Functions
@@ -35,7 +50,7 @@ new(){
 
 # Required function name "init", used in "spin init" command
 init(){
-  docker run --rm -v "$(pwd)/$project_dir:/var/www/html" --user "${SPIN_USER_ID}:${SPIN_GROUP_ID}" -e COMPOSER_CACHE_DIR=/dev/null -e "SHOW_WELCOME_MESSAGE=false" $docker_image composer --verbose --working-dir=/var/www/html/ require serversideup/spin:dev-75-spin-deploy-allow-deployments-without-cicd --dev
+  docker run --rm -v "$(pwd)/$SPIN_PROJECT_DIRECTORY:/var/www/html" --user "${SPIN_USER_ID}:${SPIN_GROUP_ID}" -e COMPOSER_CACHE_DIR=/dev/null -e "SHOW_WELCOME_MESSAGE=false" $docker_image composer --verbose --working-dir=/var/www/html/ require serversideup/spin:dev-75-spin-deploy-allow-deployments-without-cicd --dev
 }
 
 ###############################################
