f96ed8a46b8fb4e98dbc53295f58ca34b1aad9f6
Jay Rogers <jay@521dimensions.com>
2024-05-10 12:13:49 -0500

    chore: Update install.sh script to use alpine-based PHP image and require serversideup/spin:dev-75-spin-deploy-allow-deployments-without-cid --dev

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ new(){
 
 # Required function name "init", used in "spin init" command
 init(){
-  docker run --rm -v "$(pwd)/$project_dir:/var/www/html" --user "${SPIN_USER_ID}:${SPIN_GROUP_ID}" -e COMPOSER_CACHE_DIR=/dev/null -e "SHOW_WELCOME_MESSAGE=false" $docker_image composer --working-dir=/var/www/html/ require serversideup/spin:75-spin-deploy-allow-deployments-without-cicd --dev
+  docker run --rm -v "$(pwd)/$project_dir:/var/www/html" --user "${SPIN_USER_ID}:${SPIN_GROUP_ID}" -e COMPOSER_CACHE_DIR=/dev/null -e "SHOW_WELCOME_MESSAGE=false" $docker_image composer --working-dir=/var/www/html/ require serversideup/spin:dev-75-spin-deploy-allow-deployments-without-cid --dev
 }
 
 ###############################################
