4cb35b06a3e1f066dc66ad913008503ae0e38359
Jay Rogers <jay@521dimensions.com>
2024-08-02 16:51:10 -0500

    chore: Update post-install.sh to use SPIN_PROJECT_DIRECTORY instead of $project_dir

--- post-install.sh
+++ post-install.sh
@@ -4,6 +4,6 @@ prompt_and_update_file \
     --title "Configure Let's Encrypt" \
     --details "Let's Encrypt requires an email address to send notifications about SSL renewals." \
     --prompt "Please enter your email" \
-    --file "$project_dir/.infrastructure/conf/traefik/prod/traefik.yml" \
+    --file "$SPIN_PROJECT_DIRECTORY/.infrastructure/conf/traefik/prod/traefik.yml" \
     --search-default "changeme@example.com" \
     --success-msg "Updated \".infrastructure/conf/traefik/prod/traefik.yml\" with your email."
